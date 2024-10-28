a7b32610a6b76cbd13f1799e369c53366098ddf6
Felix Staudigl <felix.staudigl@ice.rwth-aachen.de>
2024-10-28 13:46:02 +0100

    forked from https://gitlab.com/norbertwalter67/windsensor_yachta.git, cleanup, restructured

--- Diagnostic Tool/Readme.md
+++
@@ -1,3 +0,0 @@
-**20190428 WiFi_Windsensor_Diagnostic_Tool_V1.00.zip {+ (stable) +}**
-
-* First version of Diagnostic Tool
--- doxygen/code/display.c
+++
@@ -1,7 +0,0 @@
-#include "display.h"
-#include <stdio.h>
-
-int displayMessage(char * message) {
-	printf(message);
-	return EXIT_SUCCESS;
-}
